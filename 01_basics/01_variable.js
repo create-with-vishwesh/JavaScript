da16e4feb089d41aa590323f660915b4d2af3fa2
create-with-vishwesh <vishweshpanchal1708@gamil.com>
2025-10-29 20:09:19 +0530

    variable undefined

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,10 +3,10 @@ let accountEmail = "vishwesh@gmail.com"
 var accountPassword = "12345"
 accountCity = "Ahemedabad"
 
+let accountState;
+
 console.log(accountId);
 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
-accountId = 11223344;
-
-// console.log(accountId); // Assignment to constant variable.
+// accountId = 11223344; // Assignment to constant variable.
