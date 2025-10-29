05ad83f77b19b13d6e1778d8fb73af7ada891d4c
create-with-vishwesh <vishweshpanchal1708@gamil.com>
2025-10-29 20:04:38 +0530

    const, let and var

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 123456
+let accountEmail = "vishwesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahemedabad"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+accountId = 11223344;
+
+// console.log(accountId); // Assignment to constant variable.
