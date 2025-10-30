dbbed7bd769fc74fa7c1eb8e26fa0219826807bf
create-with-vishwesh <vishweshpanchal1708@gamil.com>
2025-10-30 15:02:19 +0530

    dataTypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we cannot directly use it in Node js it is not browser 
+
+// console.log(3+3); console.log("Vishwesh")  // Code Readabiltiy is Less
+
+console.log(3+3);
+console.log("Vishwesh");
+
+/* Code Readability must be High */
+
+/*
+ * Documentations: mdn , tc39
+    these defines the standards for js.
+*/
+
+/* dataTypes
+    1. number -> 2 to power 52 or 53
+        |---> bit int (if needed)    
+    2. string -> used in "" or '' 
+    3. boolean -> true/false
+    4. null -> standalone value 
+            -> representation of empty value
+    5. undefined -> value not defined
+    6. symbol -> to find the uniqeness 
+                (will be mostly used in React)
+
+    7. object
+ */
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
